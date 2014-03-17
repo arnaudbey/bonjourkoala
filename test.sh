15279ab077398235f781a57ee8b66d771845d714
arnaudbey <arnaud.bey@gmail.com>
2014-03-17 16:44:38 +0100

    1st commit

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+BASE64="`wget -qO- http://www.bonjourmadame.fr | egrep -o 'c="http://.*.media.tumblr.com/[^"'"'"']*"' | sed -e 's/c=//' -e 's/["'"'"']//g' | base64 -w 0`"
+echo "...\n\n\n\033[31mBONJOUR MADAME ! \033[0m (et merci Arnaud)\nfap fap fap fap\n\n\n..."
+firefox http://koalastothemax.com/?$BASE64 &
